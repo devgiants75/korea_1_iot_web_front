3cc1e735d14afe2b92ff9780fa2f4df396249c95
devgiants75 <devgiants75@naver.com>
2024-08-05 15:07:43 +0900

    20240805

--- c_javascript/f_함수/practice01.js
+++ c_javascript/f_함수/practice01.js
@@ -2,10 +2,22 @@
 
 //! 문제 1
 // add(함수 선언식)
+function add(a, b) {
+  return a + b;
+}
+
 // subtract(함수 표현식)
+const subtract = function(a, b) {
+  return a - b;
+}
+
 // multiply(화살표 함수)
+const multiply = (a, b) => a * b;
 
 // a = 10, b = 5 (매개변수와 인자값 구분)
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
 
 //! 문제 2
 // 함수의 재사용성과 모듈성 확인 문제
@@ -28,3 +40,26 @@
 // 직원 A 급여: $계산된 금액
 // 직원 B 급여: $계산된 금액
 // 직원 C 급여: $계산된 금액
+
+function calculatePay(hours, rate) {
+  // 월 급여: 근무 시간(hours) * 시급(rate)
+
+  if (hours > 160) {
+    // 160 이하인 근무시간의 급여 계산
+    const regularPay = 160 * rate;
+
+    // 160 초과인 근무시간의 급여 계산
+    const overtimeHours = hours - 160;
+    const overtimePay = overtimeHours * rate * 1.5;
+    
+    return regularPay + overtimePay;
+
+  } else {
+    return hours * rate;
+  }
+}
+
+// 직원들의 급여 계산
+console.log('직원 A의 급여: $' + calculatePay(172, 20));
+console.log('직원 B의 급여: $' + calculatePay(160, 22));
+console.log('직원 C의 급여: $' + calculatePay(180, 18));
