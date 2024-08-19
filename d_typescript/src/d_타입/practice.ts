e6ce540c13b3ac7c61965187ec05075d8b11ac3c
devgiants75 <devgiants75@naver.com>
2024-08-19 16:29:29 +0900

    20240819

--- d_typescript/src/d_타입/practice.ts
+++ d_typescript/src/d_타입/practice.ts
@@ -11,7 +11,7 @@ let myVariable: MixedType;
 myVariable = '123';
 myVariable = 123;
 myVariable = false;
-// myVariable = []; - Error
+// myVariable = []; // Error
 
 //! 2. 함수 매개변수에 Union 타입 적용
 // number와 string 타입 중 하나를 매개변수로 받아, 해당 값이 number일 경우 숫자를 2배로 증가시키고, string일 경우 그대로 반환하는 함수 doubleOrNothing을 작성
