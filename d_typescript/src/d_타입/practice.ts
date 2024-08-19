80898b3c5ad91150044a0092da4d7c6dcedb2e15
devgiants75 <devgiants75@naver.com>
2024-08-19 16:37:07 +0900

    20240819

--- d_typescript/src/d_타입/practice.ts
+++ d_typescript/src/d_타입/practice.ts
@@ -16,11 +16,55 @@ myVariable = false;
 //! 2. 함수 매개변수에 Union 타입 적용
 // number와 string 타입 중 하나를 매개변수로 받아, 해당 값이 number일 경우 숫자를 2배로 증가시키고, string일 경우 그대로 반환하는 함수 doubleOrNothing을 작성
 
+function doubleOrNothing(input: number | string) {
+  if (typeof input === 'number') {
+    console.log(input * 2);
+  } else {
+    console.log(input);
+  }
+}
+
+doubleOrNothing(10); // 20
+doubleOrNothing('hello'); // hello
+
 //! 3. Union 타입과 타입 가드를 활용한 고급 예제
 // Admin과 User 타입 명시
 // - Admin은 id (number 타입)와 isAdmin (boolean 타입) 속성을, User는 id (number 타입)와 username (string 타입) 속성 포함
+type Admin = {
+  id: number;
+  isAdmin: boolean;
+}
+
+type User = {
+  id: number;
+  username: string;
+}
 // - 두 타입의 유니온 타입을 사용하여 Person 타입을 선언하고, id, isAdmin, username 중 적절한 속성을 가진 객체를 생성
+type Person = Admin | User;
+
 // - Person 타입의 객체를 매개변수로 받아 Admin인지 User인지를 구분해 출력하는 함수 identifyPerson을 작성
+function identifyPerson(person: Person) {
+  // 'in'연산자
+  // : 객체 안에 해당 속성이 있는지를 boolean 타입으로 반환
+  if ('isAdmin' in person) {
+    console.log('관리자 계정입니다.');
+  } else {
+    console.log('유저 계정입니다.');
+  }
+}
+
+const adminPerson: Person = {
+  id: 1,
+  isAdmin: true
+}
+
+const userPerson: Person = {
+  id: 2,
+  username: '이승아'
+}
+
+identifyPerson(adminPerson); // 관리자 계정입니다.
+identifyPerson(userPerson); // 유저 계정입니다.
 
 //& ============ 인터섹션 =============//
 //! 문제 1: 기본 Intersection 타입 생성
