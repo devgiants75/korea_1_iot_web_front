637e7b9546503ab02b2f7615208092c660bab7d4
devgiants75 <devgiants75@naver.com>
2024-08-21 11:08:39 +0900

    20240731

--- d_typescript/src/f_제네릭/generic04.ts
+++ d_typescript/src/f_제네릭/generic04.ts
@@ -25,3 +25,29 @@ console.log(result1, result2); // string 1000
 //# 제네릭 유틸리티 타입
 // : TS에 내장된 제네릭 타입
 // >> 기존의 타입을 변환하거나 조작하는데 사용
+
+// 1) Partial(부분적인, 선택적인)
+// >> 모든 속성을 선택적으로 만들어줌
+// >> Partial<T>
+
+interface User {
+  name: string;
+  age: number;
+}
+
+const users: {[key: number]: User} = {
+  1: {
+    name: '이승아',
+    age: 50
+  }, 
+  2: {
+    name: '이도경',
+    age: 40
+  }
+}
+
+// 사용자 데이터를 업데이트(수정)하는 함수 구현
+// >> id값을 사용하여 해당 데이터의 name 또는 age 값을 수정
+function updateUser(id: number, changes: Partial<User>) {
+
+}
