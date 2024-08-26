5779a6c6f443179c6f085a6dba6ab6c1d6da2961
devgiants75 <devgiants75@naver.com>
2024-08-26 14:46:08 +0900

    20240826

--- my-app/src/pages/a_basic/b_Component/index.tsx
+++ my-app/src/pages/a_basic/b_Component/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+// export 내보내기 가져오기
+import { Img } from './Component01';
+
+// 기본 내보내기 가져오기 (컴포넌트 명칭 수정 - Comp01)
 
 // index.tsx 파일을 컴포넌트로 사용 시
 // >> 함수명을 Index로 필수 수정
 export default function Index() {
   return (
-    <div>컴포넌트 학습 중 (0826, index 파일)</div>
+    <div>
+      컴포넌트 학습 중 (0826, index 파일)
+      <Img />
+    </div>
   )
 }
