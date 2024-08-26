d0f9225df33d79d0408c6ffcdb29433c61fc436c
devgiants75 <devgiants75@naver.com>
2024-08-26 12:22:33 +0900

    20240826

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactExample from './pages/a_basic/a_React/ReactExample';
 
+// 파일명까지 명시
+import Component01 from './pages/a_basic/b_Component/Component01';
+
+// index라는 이름을 가진 파일은 폴더명까지만 명시 가능
+import Index0826 from './pages/a_basic/b_Component';
+
 //! 리액트 프로젝트 개발 실행 명령어
 // npm run start
 // : http://localhost:3000 환경에서 실행
@@ -11,6 +17,10 @@ function App() {
       <h1>React Project</h1>
       {/* 컴포넌트 호출 <컴포넌트명 /> */}
       <ReactExample />
+      <Component01 />
+
+      {/* 폴더의 기본이 되는 컴포넌트 */}
+      <Index0826 />
       내용 수정
     </div>
   );
---
+++ my-app/src/pages/a_basic/b_Component/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// index.tsx 파일을 컴포넌트로 사용 시
+// >> 함수명을 Index로 필수 수정
+export default function Index() {
+  return (
+    <div>컴포넌트 학습 중 (0826, index 파일)</div>
+  )
+}
