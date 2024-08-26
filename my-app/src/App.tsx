6a1c71aa9b9798ade3bf02880f664c17dd352354
devgiants75 <devgiants75@naver.com>
2024-08-26 15:29:46 +0900

    20240826

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,6 +6,7 @@ import Component01 from './pages/a_basic/b_Component/Component01';
 
 // index라는 이름을 가진 파일은 폴더명까지만 명시 가능
 import Index0826 from './pages/a_basic/b_Component';
+import JSX from './pages/a_basic/c_JSX';
 
 //! 리액트 프로젝트 개발 실행 명령어
 // npm run start
@@ -21,6 +22,7 @@ function App() {
 
       {/* 폴더의 기본이 되는 컴포넌트 */}
       <Index0826 />
+      <JSX />
       내용 수정
     </div>
   );
