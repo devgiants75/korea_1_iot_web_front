4931f9818509c01ce405033c1b4c8f5c9e1542d6
devgiants75 <devgiants75@naver.com>
2024-08-26 10:49:14 +0900

    20240731

---
+++ my-app/src/pages/a_basic/a_React/ReactExample.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// 확장 플러그인
+// React Snippets 설치
+// : 리액트에서 사용할 다양한 코드 집합을 제공
+
+// EX) rfc(rfce - export 모듈이 분리)
+
+//! rfc 사용 함수형 컴포넌트 구조
+export default function ReactExample() {
+  return (
+    <div>ReactExample</div>
+  )
+}
+
+//! rfce 사용 함수형 컴포넌트 구조
+// import React from 'react'
+
+// function ReactExample() {
+//   return (
+//     <div>ReactExample</div>
+//   )
+// }
+
+// export default ReactExample
