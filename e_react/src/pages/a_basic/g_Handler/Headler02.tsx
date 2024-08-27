c36f9f17ba31b0f55a01efff0336057947f8dd22
devgiants75 <devgiants75@naver.com>
2024-08-27 14:50:18 +0900

    20240827

---
+++ e_react/src/pages/a_basic/g_Handler/Headler02.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// 자식 컴포넌트
+// : 부모 컴포넌트에서 사용되며 부모로부터 count 값을 전달받아 UI로 출력
+// >> p요소 내에 count 변수값을 작성
+
+// 부모 컴포넌트
+// : counter UI 작성
+// >> 버튼 2가지를 생성하여 클릭 시 count 값이 변경
+//   : 변경된 count값은 자식 컴포넌트에게 전달
+export default function Headler02() {
+  return (
+    <div>Headler02</div>
+  )
+}
