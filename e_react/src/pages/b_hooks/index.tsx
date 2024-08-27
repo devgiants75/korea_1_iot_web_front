fd424333763f46ccca1acc09aea18bed72480a8d
devgiants75 <devgiants75@naver.com>
2024-08-27 16:49:59 +0900

    20240827

--- e_react/src/pages/b_hooks/index.tsx
+++ e_react/src/pages/b_hooks/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import UseState01 from "./a_UseState/UseState01";
 
 // 전체 파일 정렬
 // ctrl + a: 전체 선택
@@ -9,6 +10,9 @@ function Index() {
       <h1 style={{ backgroundColor: "black", color: "white" }}>
         리액트 Hooks
       </h1>
+
+      <h2>리액트 Hooks - useState</h2>
+      <UseState01 />
     </div>
   );
 }
