5d0d845f4b1a7d5789117188d796e866e72feaff
devgiants75 <devgiants75@naver.com>
2024-08-28 16:15:20 +0900

    20240828

--- e_react/src/pages/b_hooks/index.tsx
+++ e_react/src/pages/b_hooks/index.tsx
@@ -7,6 +7,7 @@ import UseState05 from "./a_UseState/UseState05";
 import ChildComponent from "./a_UseState/ChildComponent";
 import UseState06 from "./a_UseState/UseState06";
 import UseRef01 from "./b_UseRef/UseRef01";
+import UseRef02 from "./b_UseRef/UseRef02";
 
 // 전체 파일 정렬
 // ctrl + a: 전체 선택
@@ -28,6 +29,7 @@ function Index() {
 
       <hr />
       <UseRef01 />
+      <UseRef02 />
     </div>
   );
 }
