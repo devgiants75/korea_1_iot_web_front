d899085d21c7c66502637c7b94e3ff5099462568
devgiants75 <devgiants75@naver.com>
2024-08-29 12:23:58 +0900

    20240829

--- e_react/src/pages/b_hooks/index.tsx
+++ e_react/src/pages/b_hooks/index.tsx
@@ -11,6 +11,7 @@ import Practice01 from "./a_UseState/Practice01";
 import UseRef03 from "./b_UseRef/UseRef03";
 import UseEffect01 from "./c_UseEffect/UseEffect01";
 import UseEffect02 from "./c_UseEffect/UseEffect02";
+import TodoAppLocalStorage from "./z_Todo/TodoAppLocalStorage";
 // import WebCamPractice from "./c_UseEffect/Practice01";
 
 // 전체 파일 정렬
@@ -44,6 +45,8 @@ function Index() {
       <UseEffect01 />
       <UseEffect02 />
       {/* <WebCamPractice /> */}
+
+      <TodoAppLocalStorage />
     </div>
   );
 }
