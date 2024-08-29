6b8f05015bdec47f9bcfa7215a9a28ffda1b34e1
devgiants75 <devgiants75@naver.com>
2024-08-29 15:45:59 +0900

    20240829

--- e_react/src/pages/z_todos/index.tsx
+++ e_react/src/pages/z_todos/index.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 import './Todo.css';
 
+interface TodoItem {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
 export default function Index() {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [filter, setFilter] = useState<'all' | 'active' | 'completed'>('all');
+  const nextIdRef = useRef<number>(0);
+
+  const addTodo = useCallback((text: string) => {
+    const newTodo = {
+      id: nextIdRef.current,
+      text,
+      completed: false
+    };
+
+    setTodos([ ...todos, newTodo ]);
+  }, []);
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const input = e.target as HTMLInputElement;
+      if (input.value.trim() !== '') {
+        addTodo(input.value.trim());
+        input.value = '';
+      }
+    }
+  }
+
+  const filteredTodos = useMemo((id: number) => {
+
+  }, [todos, filter]);
+
   return (
     <div>
       <h1>Hook 사용 예제</h1>
@@ -14,6 +48,7 @@ export default function Index() {
           type="text"
           className="todo-input"
           placeholder="Add a new task"
+          onKeyDown={handleKeyPress}
         />
         <div className="buttons-container">
           <button className="btn">All</button>
