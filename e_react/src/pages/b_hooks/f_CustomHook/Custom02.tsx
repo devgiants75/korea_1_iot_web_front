7fec23e825ba5eb0a7f82e7d08cfeef9d480860e
devgiants75 <devgiants75@naver.com>
2024-08-30 11:30:54 +0900

    20240830

--- e_react/src/pages/b_hooks/f_CustomHook/Custom02.tsx
+++ e_react/src/pages/b_hooks/f_CustomHook/Custom02.tsx
@@ -4,10 +4,13 @@ import { useInput } from "./useInput";
 export default function Custom02() {
   // 커스텀 훅을 사용한 input창 상태 관리
   //# 이름 입력에 대한 관리
+  const example1 = useInput(''); // const example1 = { value, 메서드들... }
+  const example2 = useInput('');
+
   const { 
-    value: title, 
-    bind: titleBind, // value 데이터 값, onChange 변화 함수
-    reset: titleReset 
+    value: name, 
+    bind: nameBind, // value 데이터 값, onChange 변화 함수
+    reset: nameReset 
   } = useInput("");
 
   //# 이메일 입력에 대한 관리
@@ -19,6 +22,9 @@ export default function Custom02() {
 
   const handleSubmit = () => {
     console.log(`회원가입 완료: `);
+
+    nameReset();
+    emailReset();
   };
 
   // useInput에 대한 호출 마다 새로운 데이터가 상태 관리
@@ -39,18 +45,22 @@ export default function Custom02() {
         name="username"
         placeholder="사용자 이름"
         // UI에 바인딩(적혀)되어 실질적으로 프로세스가 구현
-        value={titleBind.value}
-        onChange={titleBind.onChange}
+        {...nameBind}
       />
-      <button onClick={titleReset}>초기화</button>
+      <button onClick={nameReset}>이름 초기화</button>
       <input
         type="text"
         name="email"
         placeholder="사용자 이메일"
-        value={emailBind.value}
-        onChange={emailBind.onChange}
+        {...emailBind}
+        // value={emailBind.value}
+        // onChange={emailBind.onChange}
       />
       <button onClick={emailReset}>이메일 초기화</button>
+
+      {/* 사용자 별명(nickName) 입력 창 */}
+      {/* 해당 입력 창에 대한 초기화 */}
+
       <button onClick={handleSubmit}>회원가입</button>
     </div>
   );
