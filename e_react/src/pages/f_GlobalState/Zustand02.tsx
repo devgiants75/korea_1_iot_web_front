23c47eb67d2191140851036f9b0d4fdc92793b92
devgiants75 <devgiants75@naver.com>
2024-09-03 15:45:50 +0900

    20240903

--- e_react/src/pages/f_GlobalState/Zustand02.tsx
+++ e_react/src/pages/f_GlobalState/Zustand02.tsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import { useMemberStore } from '../../stores/user.store'
+import React, { useState } from "react";
+import { useMemberStore } from "../../stores/user.store";
+import { useAuthStore } from "../../stores/auth.store";
 
 export default function Zustand02() {
   const { members, addMember } = useMemberStore();
+  const { user, logout } = useAuthStore();
 
   const handleCreate = () => {
     addMember({
       id: 1,
-      name: '이승아'
+      name: "이승아",
     });
-  }
+  };
 
   return (
     <div>
-      {members.map(user => (
+      {members.map((user) => (
         <div>
-          <p>{user.id}</p>
-          <p>{user.name}</p>
+          <p>
+            {user.id} / {user.name}
+          </p>
         </div>
       ))}
+      <button onClick={() => handleCreate()}>추가</button>
+
+      <hr />
+      <p>Welcome {user}</p>
+      <Login />
+      <button onClick={logout}>로그아웃</button>
     </div>
-  )
+  );
 }
+
+//# 로그인 컴포넌트
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const { login } = useAuthStore();
+
+  return (
+    <>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={() => login(username)}>로그인</button>
+    </>
+  );
+};
