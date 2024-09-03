c56ed6017c69d1ffbe743d70490c1f575014b022
devgiants75 <devgiants75@naver.com>
2024-09-03 12:22:55 +0900

    20240903

--- e_react/src/pages/f_GlobalState/index.tsx
+++ e_react/src/pages/f_GlobalState/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 
 //! 리액트 '전역' 상태 관리
 // : 프로젝트 '전체 영역'에서 데이터를 공유할 수 있는 방법
@@ -48,10 +48,50 @@ const UserProvider = ({ children }: { children: React.ReactNode }) => {
   )
 }
 
+// 전역 상태 관리 데이터를 사용할 컴포넌트
+const ExampleHeader = () => {
+  const userContext = useContext(UserContext);
+
+  if (!userContext) {
+    throw new Error('UserContext가 UserProvider 내에서 사용되지 않음');
+  }
+  const { user, setUser } = userContext;
+
+  if (!user) {
+    return <button onClick={() => setUser({ name: '이승아' })}>로그인</button>
+  }
+
+  return (
+    <div>
+      <h1>프로젝트 헤더</h1>
+      <p>{user.name}</p>
+    </div>
+  )
+}
+
+const ExampleNaviBar = () => {
+  const userContext = useContext(UserContext);
+
+  if (!userContext) {
+    throw new Error('UserContext가 UserProvider 내에서 사용되지 않음');
+  }
+  const { user, setUser } = userContext;
+
+  return (
+    <div>
+      Hello, { user ? user.name : '게스트' }
+      <button onClick={() => setUser(null)}>로그아웃</button>
+    </div>
+  )
+}
+
 export default function Index() {
   return (
     <div>
-
+      <UserProvider>
+        <ExampleHeader />
+        <ExampleNaviBar />
+      </UserProvider>
     </div>
   )
 }
